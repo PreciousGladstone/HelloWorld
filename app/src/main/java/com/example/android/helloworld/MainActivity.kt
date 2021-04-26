ff39a3fc3d6e0dfb7e6cd3dc3aedfb3c1be14929
PreciousGladstone <preciousgladstone@gmail.com>
2021-04-26 17:40:07 +0100

    hello world mobile app

---
+++ app/src/main/java/com/example/android/helloworld/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.android.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+    fun decrement(){
+        TextView
+    }
+    fun increment(){
+
+    }
